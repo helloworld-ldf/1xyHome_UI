b9beff096949098a23574d4cb0ab718870c6a169
李代福 <helloworld_ldf@126.com>
2017-04-09 00:55:23 +0800

    Signed-off-by: 李代福 <helloworld_ldf@126.com>

---
+++ js/select.js
@@ -0,0 +1,76 @@
+// 租房 方式
+var style = [
+	{ text: "不限", value: "0" },
+	{ text: "整租", value: "1" },
+	{ text: "合租", value: "2" },
+	{ text: "日租", value: "3" }
+]
+
+// 租房户型
+var type = [
+	{ text: "不限", value: "0" },
+	{ text: "一居", value: "1" },
+	{ text: "二居", value: "2" },
+	{ text: "三居", value: "3" },
+	{ text: "四居", value: "4" },
+	{ text: "四居以上", value: "5" }
+]
+
+// 租房 特色
+var characteristic = [
+	{ text: "不限", value: "0" },
+	{ text: "近地铁", value: "1" },
+	{ text: "全新装修", value: "2" },
+	{ text: "名校旁边", value: "3" },
+	{ text: "低价城中村", value: "4" },
+	{ text: "大院管理", value: "5" },
+	{ text: "电梯房", value: "6" },
+	{ text: "单身公寓", value: "7" },
+	{ text: "随时入住", value: "8" },
+	{ text: "吉屋出租", value: "9" },
+	{ text: "南北对流", value: "10" }
+]
+
+// 教育程度
+var education = [
+	{ text: "请选择", value: "0" },
+	{ text: "初中", value: "1" },
+	{ text: "高中", value: "2" },
+	{ text: "中专", value: "3" },
+	{ text: "大专", value: "4" },
+	{ text: "本科", value: "5" },
+	{ text: "硕士", value: "6" },
+	{ text: "博士", value: "7" },
+	{ text: "其他", value: "8" }
+]
+
+// 职业
+var profession = [
+	{ text: "请选择", value: "0" },
+	{ text: "政府机关/干部", value: "1" },
+	{ text: "计算机", value: "2" },
+	{ text: "网络", value: "3" },
+	{ text: "商业/贸易", value: "4" },
+	{ text: "银行/金融/证劵/保险", value: "5" },
+	{ text: "税务", value: "6" },
+	{ text: "咨询", value: "7" },
+	{ text: "服务", value: "8" },
+	{ text: "旅游/饭店", value: "9" },
+	{ text: "房地产", value: "10" },
+	{ text: "法律/司法", value: "11" },
+	{ text: "文化/教育", value: "12" },
+	{ text: "媒介/广告", value: "13" },
+	{ text: "农/渔/林/畜牧业", value: "14" },
+	{ text: "矿业/制造业", value: "15" },
+	{ text: "学生", value: "16" },
+	{ text: "自由职业", value: "17" },
+	{ text: "其他", value: "18" }
+]
+
+// 证件类型
+var papersType = [
+	{ text: "身份证", value: "0" },
+	{ text: "工作证", value: "1" },
+	{ text: "军官证", value: "2" },
+	{ text: "学生证", value: "3" }
+]
